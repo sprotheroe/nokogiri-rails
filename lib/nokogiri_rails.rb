b41fa4ad912d53979c151c6d5e5910a79780d217
Simon Protheroe <protheroe@gmail.com>
2024-02-14 17:14:03 +1000

    Initial commit for generated files

---
+++ lib/nokogiri_rails.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "nokogiri_rails/version"
+
+module NokogiriRails
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/nokogiri_rails/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module NokogiriRails
+  VERSION = "0.1.0"
+end
